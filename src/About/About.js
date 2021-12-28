a926cda574e1c7c869c08a7cff1f655997d8f14d
HenrikMoe <henrikmoe29@gmail.com>
2021-12-28 10:38:40 -0800

    link

--- src/About/About.js
+++ src/About/About.js
@@ -36,7 +36,7 @@ const About = () => {
 				CryptoCount supports 40 countries and 1 blockchain. 
                 <br />
 				<br />
-                The project supports data integration to an existing app. Pre-built react components and API documentation are located in the <a href="https://www.portaltoblockchain.org/about">Client Module</a> and <a href="https://www.portaltoblockchain.org/api">API</a> sections of this site. 
+                The project supports data integration to an existing app. Pre-built react components and API documentation are located in the <a href="https://www.portaltoblockchain.org/client">Client Module</a> and <a href="https://www.portaltoblockchain.org/api">API</a> sections of this site. 
                 <br />
 				<br />
                 The software was designed from economic literature authored by Sutherland and Landoni(2020), listed in the <a href="https://www.portaltoblockchain.org/literature">Academic Literature</a> section of this site. 
