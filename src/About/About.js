f877afd240c9ca03682deec47030d8eb674292e5
HenrikMoe <henrikmoe29@gmail.com>
2021-12-28 10:23:17 -0800

    about

--- src/About/About.js
+++ src/About/About.js
@@ -21,25 +21,25 @@ const About = () => {
 				</h1>
                   <br />
 				<br />
-		CryptoCount supplies tax optimized financial data and accounting services for Tezos block reward delegators.				
+		CryptoCount supplies tax optimized financial data to Tezos block reward delegators.				
                 <br />
 				<br />
-		The website visualizes block rewards and delivers income statements to users.	
+		The website visualizes block rewards and generates income statements to users.	
 		<br />
 				<br />
                 Statements are generated when a user selects a quantity of rewards to realize.
                 <br />
 				<br />
-                CryptoCount supports 40 countries and 1 blockchain. 
+				Downloadable statements can be delivered to authorities for tax reporting. 
                 <br />
 				<br />
-                Downloadable statements can be delivered to authorities for tax reporting. 
+				CryptoCount supports 40 countries and 1 blockchain. 
                 <br />
 				<br />
-                This project supports data integration to an existing app. Pre-built react components and API documentation are located in the <a href="https://www.portaltoblockchain.org/about">Client Module</a> and <a href="https://www.portaltoblockchain.org/api">API</a> sections of this site. 
+                The project supports data integration to an existing app. Pre-built react components and API documentation are located in the <a href="https://www.portaltoblockchain.org/about">Client Module</a> and <a href="https://www.portaltoblockchain.org/api">API</a> sections of this site. 
                 <br />
 				<br />
-                The software was designed from literature authored by Sutherland and Landoni(2020), listed in the <a href="https://www.portaltoblockchain.org/literature">Academic Literature</a> section of this site. 
+                The software was designed from economic literature authored by Sutherland and Landoni(2020), listed in the <a href="https://www.portaltoblockchain.org/literature">Academic Literature</a> section of this site. 
                 <br />
 				<br />
 				Visit our <a href="https://www.github.com/PortalToBlockchainOrganization">GitHub</a> and feel free to leave feature requests. 
