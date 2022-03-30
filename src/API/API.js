1d799dd79b47b7a459b4a779a10f103afe5da030
HenrikMoe <henrikmoe29@gmail.com>
2022-03-30 15:43:28 -0700

    urls

--- src/API/API.js
+++ src/API/API.js
@@ -141,7 +141,7 @@ const API = () => {
 								url="http://api.portaltoblockchain.org/Analysis/Tezos/Auto"
 								params={"?address=${address}&fiat=${fiat}"}
 								type="get"
-								exampleReq="http://api.portaltoblockchain.org/Analysis/Tezos/AutoRealize?address=tz1TzS7MEQoCT6rdc8EQMXiCGVeWb4SLjnsH&fiat=USD&realizingQuantity=1000"
+								exampleReq="http://api.portaltoblockchain.org/Analysis/Tezos/Auto?address=tz1TzS7MEQoCT6rdc8EQMXiCGVeWb4SLjnsH&fiat=USD"
 								exampleRes={{	address:
 									"tz1TzS7MEQoCT6rdc8EQMXiCGVeWb4SLjnsH",
 								basisPrice: 3.792666426254427,
